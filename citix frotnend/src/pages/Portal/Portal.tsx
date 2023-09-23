2a6a76d0f49eb17bebe766bbdc2bef578ea2978d
mukayevolzhas <olzhas.mukayev@nu.edu.kz>
2023-09-24 01:17:29 +0600

    asd

--- citix frotnend/src/pages/Portal/Portal.tsx
+++ citix frotnend/src/pages/Portal/Portal.tsx
@@ -158,9 +158,9 @@ const Portal: React.FC<MatchParams> = (props) => {
   return (
     <HomeLayout>
       <Header />
-      <div className="w-full rounded-2xl bg-info-card">
+      <div className="w-full h-fit rounded-2xl bg-info-card relative">
         <video
-          className="hidden"
+          className="absolute top-0 right-0 h-1/3 w-1/3 rounded-tl-2xl rounded-bl-2xl"
           autoPlay
           controls={false}
           ref={userVideo}
